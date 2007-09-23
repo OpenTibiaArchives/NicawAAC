8b9736a5dd50bdab7d2f648645a0e328b89cc9d5
nicaw <nicaw@5f632a7e-2b37-0410-85a5-88609aea1598>
2007-09-23 09:57:51 +0000

    * Poll table normalized

--- trunk/sql/database.sql
+++ trunk/sql/database.sql
@@ -1,3 +1,26 @@
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `nicaw_accounts`
+-- 
+
+CREATE TABLE `nicaw_accounts` (
+  `accno` int(11) unsigned NOT NULL,
+  `email` varchar(50) collate latin1_general_ci NOT NULL,
+  `rlname` varchar(50) collate latin1_general_ci NOT NULL default '',
+  `location` varchar(50) collate latin1_general_ci NOT NULL default '',
+  `ip` varchar(16) collate latin1_general_ci NOT NULL default '',
+  `blocked` tinyint(1) NOT NULL default '0',
+  `comment` tinytext collate latin1_general_ci NOT NULL,
+  KEY `accno` (`accno`)
+);
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `nicaw_logs`
+-- 
+
 CREATE TABLE `nicaw_logs` (
   `id` int(11) unsigned NOT NULL auto_increment,
   `ip` varchar(16) collate latin1_general_ci NOT NULL,
@@ -9,6 +32,10 @@ CREATE TABLE `nicaw_logs` (
 );
 
 -- --------------------------------------------------------
+
+-- 
+-- Table structure for table `nicaw_news`
+-- 
 
 CREATE TABLE `nicaw_news` (
   `id` int(11) unsigned NOT NULL auto_increment,
@@ -22,19 +49,50 @@ CREATE TABLE `nicaw_news` (
 
 -- --------------------------------------------------------
 
-CREATE TABLE `nicaw_polls` (
-  `id` int(11) unsigned NOT NULL auto_increment,
-  `question` varchar(225) collate latin1_general_ci NOT NULL,
-  `options` varchar(225) collate latin1_general_ci NOT NULL,
-  `results` varchar(128) collate latin1_general_ci NOT NULL,
-  `startdate` int(11) NOT NULL,
-  `enddate` int(11) NOT NULL,
-  `minlevel` int(11) NOT NULL default '1',
+-- 
+-- Table structure for table `nicaw_poll_options`
+-- 
+
+CREATE TABLE `nicaw_poll_options` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `poll_id` int(10) unsigned NOT NULL,
+  `option` varchar(255) collate latin1_general_ci NOT NULL,
+  UNIQUE KEY `id` (`id`)
+);
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `nicaw_poll_questions`
+-- 
+
+CREATE TABLE `nicaw_poll_questions` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `question` varchar(255) collate latin1_general_ci NOT NULL,
+  `startdate` int(10) unsigned NOT NULL,
+  `enddate` int(10) unsigned NOT NULL,
+  `minlevel` int(10) unsigned NOT NULL,
   `hidden` tinyint(1) NOT NULL default '0',
   UNIQUE KEY `id` (`id`)
 );
 
 -- --------------------------------------------------------
+
+-- 
+-- Table structure for table `nicaw_poll_votes`
+-- 
+
+CREATE TABLE `nicaw_poll_votes` (
+  `option_id` int(11) unsigned NOT NULL,
+  `accno` int(11) NOT NULL,
+  `ip` varchar(16) collate latin1_general_ci NOT NULL
+);
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `nicaw_recovery`
+-- 
 
 CREATE TABLE `nicaw_recovery` (
   `accno` int(11) unsigned NOT NULL,
@@ -43,14 +101,6 @@ CREATE TABLE `nicaw_recovery` (
   `ip` varchar(16) collate latin1_general_ci NOT NULL default '',
   `key` char(32) collate latin1_general_ci NOT NULL,
   KEY `accno` (`accno`)
-);
-
--- --------------------------------------------------------
-
-CREATE TABLE `nicaw_votes` (
-  `id` int(11) unsigned NOT NULL,
-  `accno` int(11) NOT NULL,
-  `ip` varchar(16) collate latin1_general_ci NOT NULL
 );
 
 -- --------------------------------------------------------
